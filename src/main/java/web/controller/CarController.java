f3b313a03e141c80d5422786a34be3e0b3350216
Zaha <badi.zaha@gmail.com>
2024-02-17 12:11:53 +0200

    Task 2.2.2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -13,44 +13,15 @@ import java.util.Optional;
 public class CarController {
     @GetMapping(value = "/car" )
     public String printWelcome(ModelMap model,
-                               @RequestParam(value = "all", required = false) String all,
                                @RequestParam(value = "count", required = false) Integer count) {
         CarService carService = new CarService();
-        all="";
-        if(count == null || count <=0|| count >5 || all.equals("all")){
+        if(count == null || count <=0|| count >5){
             model.addAttribute("carList", carService.listCars());
         }
         else{
             model.addAttribute("carList", carService.listCars(count));
         }
-
-
-
         return "car";
     }
-//    @RequestMapping(value = "/car?count={count}" ,method = RequestMethod.GET)
-//    public String printWelcome(ModelMap model) {
-//        String count = "1";
-//        System.out.println(count);
-//        if(Integer.parseInt(count) == 0 || Integer.parseInt(count) > 5  ){
-//            List<String> messages = new ArrayList<>();
-//            messages.add("Congratulations!");
-//            messages.add("You just launched a Car :)");
-//            CarService carService = new CarService();
-//
-//            model.addAttribute("carList",  carService.listCars(Integer.parseInt(count)));
-//            return "car";
-//        }
-//        else{
-//            List<String> messages = new ArrayList<>();
-//            messages.add("Congratulations!");
-//            messages.add("You just launched a Car :)");
-//            CarService carService = new CarService();
-//
-//            model.addAttribute("carList", carService.listCars());
-//            return "car";
-//
-//        }
 
-//    }
 }
