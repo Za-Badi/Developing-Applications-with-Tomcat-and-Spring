259c618d6dcb197574ddfb4b4f0a4f2e80782d60
Zaha <badi.zaha@gmail.com>
2024-02-23 11:41:17 +0200

    Task 2.2.3

--- src/main/java/hiber/dao/UserDao.java
+++ src/main/java/hiber/dao/UserDao.java
@@ -8,10 +8,7 @@ public interface UserDao {
    void add(User user);
    List<User> listUsers();
    void update(User user);
-//   void saveUser(String name, String lastName, byte age);
-
    void removeUserById(long id);
-//   void updateUser(User user);
 
 
 
